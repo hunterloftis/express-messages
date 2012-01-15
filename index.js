ec8a401d66cb9f53ec8e6d0dfba7c1513a328d15
Hunter Loftis <hunter+github@hunterloftis.com>
2012-01-15 17:47:42 -0500

    Modified to use Twitter Bootstrap css

--- index.js
+++ index.js
@@ -12,20 +12,19 @@ module.exports = function(req, res){
       , types = Object.keys(messages)
       , len = types.length;
     if (!len) return '';
-    buf.push('<div id="messages">');
     for (var i = 0; i < len; ++i) {
       var type = types[i]
         , msgs = messages[type];
       if (msgs) {
-        buf.push('  <ul class="' + type + '">');
+        buf.push('  <div class="alert-message ' + type + '">');
+        buf.push('    <a class="close" href="#">x</a>');
         for (var j = 0, l = msgs.length; j < l; ++j) {
           var msg = msgs[j];
-          buf.push('    <li>' + msg + '</li>');
+          buf.push('    <p>' + msg + '</p>');
         }
-        buf.push('  </ul>');
+        buf.push('  </div>');
       }
     }
-    buf.push('</div>');
     return buf.join('\n');
   }
 };
